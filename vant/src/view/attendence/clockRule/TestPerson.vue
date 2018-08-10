c214c10ea2852a840920393e527883f5df66f4af
chendan <603446367@qq.com>
2018-08-10 15:28:23 +0800

    ceshi

--- vant/src/view/attendence/clockRule/TestPerson.vue
+++ vant/src/view/attendence/clockRule/TestPerson.vue
@@ -209,4 +209,18 @@ function getChildrenByPid(id, items) {
   return result;
 }
 
+function getPidByCurrentId(id,items){
+  let result;
+  for (var i in items) {
+    let item = items[i];
+    if (item.id == id) {
+      result = item.children;
+      break;
+    } else if (item.children) {
+      result = getCurrentPart(id, item.children);
+    }
+  }
+  return result;
+}
+
 </script>
