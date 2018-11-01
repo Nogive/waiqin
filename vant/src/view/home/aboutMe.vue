b01e1af8af32d750d6491593679eafafcab18422
chendan <603446367@qq.com>
2018-11-01 12:49:25 +0800

    vconsole

--- vant/src/view/home/aboutMe.vue
+++ vant/src/view/home/aboutMe.vue
@@ -5,8 +5,8 @@
       <van-cell title="版本信息" value="1.1.0" label="当前为测试版本。" />
       <van-cell title="是否开启调试模式">
         <template slot="right-icon">
-          <van-button v-if="bugOn" type="primary" plain size="small" @click="creatScript">开启</van-button>
-          <van-button v-else type="primary" plain size="small" @click="creatScript">关闭</van-button>
+          <van-button v-if="bugOn" type="primary" plain size="small" @click="openVconsole">开启</van-button>
+          <van-button v-else type="primary" plain size="small" @click="closeVconsole">关闭</van-button>
         </template>
       </van-cell>
     </van-cell-group>
@@ -91,13 +91,20 @@ export default {
       };
       accountApi.login(body, callback);
     },
-    creatScript() {
+    openVconsole() {
+      let _this=this;
       let url =
         "http://maimang-public.oss-cn-hangzhou.aliyuncs.com/vconsole.min.js";
       createScript(url, function() {
         var vConsole = new VConsole();
+        Vue.prototype.vConsole=vConsole;
+        _this.bugOn=false;
         console.log("aaaaa");
       });
+    },
+    closeVconsole(){
+      this.vConsole.destroy();
+      this.bugOn=true;
     }
   }
 }
